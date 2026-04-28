1fa649a30b544f4d49695227e248ed87dba028b6
vicky <tyagivivek528@gmail.com>
2026-04-28 18:49:37 +0530

    rag indexing done using langchain and qdrant db

--- 8_pdf_agent/main.py
+++ 8_pdf_agent/main.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from dotenv import load_dotenv
 
+load_dotenv()
 pdf_path = Path(__file__).parent / "golang.pdf"
 
 # load the pdf
@@ -8,4 +13,22 @@ loader = PyPDFLoader(file_path=pdf_path)
 
 docs = loader.load()
 
-print(docs[43])
+# print(docs[43])
+
+# implemeting chunking
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=400)
+chunks = text_splitter.split_documents(documents=docs)
+
+# print(chunks[0])
+
+embeddings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-2-preview")
+# vector = embeddings.embed_query("hello, world!")
+
+vector_store = QdrantVectorStore.from_documents(
+    embedding=embeddings,
+    documents=chunks,
+    url="http://localhost:6333",
+    collection_name="learning_rag",
+)
+
+print("Indexing of doc is done.👍🏿")
